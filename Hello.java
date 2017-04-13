a2e6a75be5f4247fbca13b92deffb6947e8f8675
Heensaem Lee <dgtoa0@gmail.com>
2017-04-14 01:42:52 +0900

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
-class Hello {
+//Hello.java
+public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello, world from GIT!");
+    System.out.println("Changes after First commit!");    
   }
 }
